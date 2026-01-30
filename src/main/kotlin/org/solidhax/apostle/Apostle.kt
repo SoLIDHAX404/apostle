c1007e7791f7ae28362559fceef35cf376b24552
SoLID_HAX404 <toastbrotgaming@gmx.at>
2026-01-30 20:29:13 +0100

    Initial Commit

---
+++ src/main/kotlin/org/solidhax/apostle/Apostle.kt
@@ -0,0 +1,9 @@
+package org.solidhax.apostle
+
+import net.fabricmc.api.ModInitializer
+
+class Apostle : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/org/solidhax/apostle/client/ApostleClient.kt
@@ -0,0 +1,9 @@
+package org.solidhax.apostle.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class ApostleClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
