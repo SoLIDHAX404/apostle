4c1371186a196cc592addc343bff315582d1c617
SoLID_HAX404 <toastbrotgaming@gmx.at>
2026-02-07 15:14:45 +0100

    Revert "Merge pull request #1 from SoLIDHAX404/codex/add-grid-layout-for-module-rendering"
    This reverts commit 4c3c9f568dad0530a19ae4b0ebecaa9f14ab111b, reversing
    changes made to f36afc1a11e0fe85640d54d2af93ecb88e87cbc2.

--- src/main/kotlin/org/solidhax/apostle/gui/clickgui/ClickGUI.kt
+++ src/main/kotlin/org/solidhax/apostle/gui/clickgui/ClickGUI.kt
@@ -5,25 +5,16 @@ import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.input.MouseButtonEvent
 import net.minecraft.network.chat.Component
 import org.solidhax.apostle.gui.clickgui.components.UIWindow
-import org.solidhax.apostle.gui.clickgui.pages.ClickGuiPage
-import org.solidhax.apostle.gui.clickgui.pages.ModuleDetailPage
-import org.solidhax.apostle.gui.clickgui.pages.ModuleGridPage
-import org.solidhax.apostle.gui.clickgui.pages.UIBounds
 import org.solidhax.apostle.utils.render.NVGSpecialRenderer
 
 object ClickGUI : Screen(Component.literal("Click GUI")) {
 
     val mainWindow = UIWindow(0f, 0f, 600f, 400f, "Click GUI Window")
-    private val contentPadding = 12f
-    private var currentPage: ClickGuiPage = ModuleGridPage { module ->
-        currentPage = ModuleDetailPage(module)
-    }
 
     override fun render(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, deltaTicks: Float) {
 
         NVGSpecialRenderer.draw(guiGraphics, 0, 0, guiGraphics.guiWidth(), guiGraphics.guiHeight()) {
             mainWindow.render(guiGraphics, mouseX, mouseY, deltaTicks)
-            currentPage.render(guiGraphics, contentBounds(), mouseX, mouseY, deltaTicks)
         }
 
         super.render(guiGraphics, mouseX, mouseY, deltaTicks)
@@ -31,13 +22,11 @@ object ClickGUI : Screen(Component.literal("Click GUI")) {
 
     override fun mouseClicked(mouseButtonEvent: MouseButtonEvent, bl: Boolean): Boolean {
         mainWindow.mouseClicked(mouseButtonEvent, bl)
-        currentPage.mouseClicked(mouseButtonEvent, contentBounds())
         return super.mouseClicked(mouseButtonEvent, bl)
     }
 
     override fun mouseReleased(mouseButtonEvent: MouseButtonEvent): Boolean {
         mainWindow.mouseReleased(mouseButtonEvent)
-        currentPage.mouseReleased(mouseButtonEvent, contentBounds())
         return super.mouseReleased(mouseButtonEvent)
     }
 
@@ -46,13 +35,4 @@ object ClickGUI : Screen(Component.literal("Click GUI")) {
     override fun renderBackground(guiGraphics: GuiGraphics?, i: Int, j: Int, f: Float) { return }
 
     override fun isPauseScreen(): Boolean { return false}
-
-    private fun contentBounds(): UIBounds {
-        return UIBounds(
-            mainWindow.x + contentPadding,
-            mainWindow.y + contentPadding,
-            mainWindow.width - (contentPadding * 2),
-            mainWindow.height - (contentPadding * 2),
-        )
-    }
 }
