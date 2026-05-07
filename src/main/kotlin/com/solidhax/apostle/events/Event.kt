e73e8817f34e026723984b5375019a11d661aedb
SoLIDHAX404 <toastbrotgaming@gmx.at>
2026-05-07 08:46:04 +0200

    internal: added playerMessage and error handling for the EventBus

--- src/main/kotlin/com/solidhax/apostle/events/Event.kt
+++ src/main/kotlin/com/solidhax/apostle/events/Event.kt
@@ -1,10 +1,16 @@
 package com.solidhax.apostle.events
 
 import com.solidhax.apostle.Apostle.bus
+import com.solidhax.apostle.utils.errorMessage
 
 interface Event {
     fun post(): Boolean {
-        bus.post(this)
+        runCatching {
+            bus.post(this)
+        }.onFailure {
+            errorMessage(it, this)
+        }
+
         return false
     }
 }
--- src/main/kotlin/com/solidhax/apostle/utils/ChatUtils.kt
+++ src/main/kotlin/com/solidhax/apostle/utils/ChatUtils.kt
@@ -1,1 +1,16 @@
 package com.solidhax.apostle.utils
+
+import com.solidhax.apostle.Apostle.mc
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
+
+fun playerMessage(message: Any?, prefix: String = "§7[§5Apostle§7] ", chatStyle: Style? = null) {
+    val text = Component.literal("$prefix$message")
+    chatStyle?.let { text.setStyle(chatStyle) }
+    mc.execute { mc.gui.chat.addMessage(text) }
+}
+
+fun errorMessage(throwable: Throwable, context: Any) {
+    val message = "§cCaught an ${throwable.javaClass.simpleName} at ${context.javaClass.simpleName}"
+    playerMessage(message)
+}
